0a70bd1ab6f3a3b5926590e50fe9f211421b66d9
alex <acontell@gmail.com>
2021-08-30 09:07:05 -0400

    MFailing one test

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,7 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
@@ -13,9 +14,30 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/) && @@cart.length > 0
+      @@cart.each do |item|
+        resp.write "#{item}\n"
+      end
+    elsif req.path.match(/cart/) && @@cart.length == 0
+        resp.write "Your cart is empty"
+
+    else req.path.match(/add/) 
+
+      item_name = req.path.split("/songs/").last 
+
+      item = @@items.find{|s| s == item_name}
+
+      if(item == nil)
+        resp.write "We don't have that item"
+      elsif
+        item = item_name.capitalize
+        @@cart << item
+        resp.write "added #{item}"
+
     else
       resp.write "Path Not Found"
     end
+  end
 
     resp.finish
   end
@@ -27,4 +49,9 @@ class Application
       return "Couldn't find #{search_term}"
     end
   end
+  private
+
+  # def params_id
+  #   req.path.split("/add/").last
+  # end
 end
